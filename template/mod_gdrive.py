f080798ac79ba8e3d64056ebd748d7940c59f2ec
MakarMS <76819996+MakarMS@users.noreply.github.com>
2020-12-31 20:16:26 +0300

    Update mod_gdrive.py

--- template/mod_gdrive.py
+++ template/mod_gdrive.py
@@ -6,9 +6,9 @@ C = '\033[36m' # cyan
 W = '\033[0m'  # white
 
 redirect = input(G + '[+]' + C + ' Enter GDrive File URL : ' + W)
-with open('template/gdrive/js/location_temp.js', 'r') as js:
+with open('template/gdrive/location_temp.js', 'r') as js:
 	reader = js.read()
 	update = reader.replace('REDIRECT_URL', redirect)
 
-with open('template/gdrive/js/location.js', 'w') as js_update:
+with open('template/gdrive/location.js', 'w') as js_update:
 	js_update.write(update)
